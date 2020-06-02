cec096bb10519b8141421b70200471f2238677f8
kyle Dornan <kyledornan26@gmail.com>
2020-06-02 14:44:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(num)
+  num.sort do |a,b|
+    a<=>b 
+  end 
+end 
 
+
+def sort_array_desc(num)
+  num.sort do |a,b|
+  b<=>a
+  end
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a,b| 
+a.length<=>b.length 
+ end 
+end 
+
+
+def swap_elements(array)
+  array[1],array[2]=array[2],array[1]
+  array
+end 
+
+def reverse_array(num)
+num.reverse
+end
+
+def kesha_maker(array)
+new_array=[]
+array.each do |name|
+   name[2]="$"
+end 
+  end 
+  
+  def find_a(array)
+    array.each do |name|
+  if name.start_with?("a")
+    array 
+end 
+end 
+  end 
+  
+  
